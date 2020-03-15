844a17a96829fcde71ff1d655a7ee8ff2dec0ed1
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-15 12:56:59 +0000

    Added checkbox and radio things

--- NHSCheckbox.js
+++ NHSCheckbox.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function NHSCheckbox(props) {
+    //TODO: key
     return <div className="nhsuk-checkboxes__item" style={props.style}>
-        <input className="nhsuk-checkboxes__input" type="checkbox" name={props.name} value={props.value}/>
-            <label className="nhsuk-label nhsuk-checkboxes__label">
-                {props.label}
-            </label>
+        <input className="nhsuk-checkboxes__input" type="checkbox" name={props.name} value={props.value}
+               onChange={props.onChange}/>
+        <label className="nhsuk-label nhsuk-checkboxes__label">
+            {props.label}
+        </label>
     </div>
-;
+        ;
 }
 
 export default NHSCheckbox;
---
+++ NHSRadioGroup.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NHSFormGroup, NHSFormHint } from "./NHSComponents";
+
+export function NHSFieldsetLegend(props) {
+    return <legend className="nhsuk-fieldset__legend" {...props} />;
+}
+
+export function NHSFieldset(props) {
+    return <fieldset className="nhsuk-fieldset" {...props} />;
+}
+
+function NHSRadioGroup(props) {
+    return <NHSFormGroup>
+        <NHSFieldset className="nhsuk-fieldset">
+            <NHSFieldsetLegend>{props.label}</NHSFieldsetLegend>
+            <NHSFormHint>{props.help}</NHSFormHint>
+            <div className="nhsuk-radios">
+                {props.options.map((option) => {
+                    return <div className="nhsuk-radios__item">
+                        <input className="nhsuk-radios__input" name={props.name} type="radio"
+                               value={option.value}/>
+                        <label className="nhsuk-label nhsuk-radios__label">
+                            {option.label}
+                        </label>
+                    </div>;
+                })}
+            </div>
+        </NHSFieldset>
+    </NHSFormGroup>
+        ;
+}
+
+export default NHSRadioGroup;
